f93ccbb6d4e698649d8deb959eb5a775eaa1f7ec
Aline Barros <baline1225@gmail.com>
2024-07-11 16:31:53 -0300

    Inserindo validações no IMC

--- js/principal.js
+++ js/principal.js
@@ -24,14 +24,40 @@
     console.log(altura);
 
     var tdImc = paciente.querySelector(".info-imc");
+
+    // iniciando variaveis como verdadeiras
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    //validação 
+    if(peso <= 0  || peso >= 1000) {
+        console.log("Peso invalido");
+        //inserindo falso se obedecer a condição do if
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!";
+    }
+
+    if(altura <= 0  || altura >= 3.00) {
+        console.log("Altura invalido");
+        alturaEhValida = false
+        tdImc.textContent = "Altura invalida!";
+    }
+
+
+    if (alturaEhValida && pesoEhValido) {
+
     //----- calculo do imc
     //coloco () no calculo que eu quero que faça primeiro
     var imc = peso / (altura * altura);
     console.log(imc);
 
-
     tdImc.textContent = imc;
-    
+    }
+
+    else {
+        console.log("Ajuste a altura ou o peso")
+    }
+
 
 
 
